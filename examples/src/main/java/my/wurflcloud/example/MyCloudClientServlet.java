25b22575dd412417247f43a09942dccb95afc865
andrea <castello.a@gmail.com>
2021-06-08 14:51:52 +0200

    Example app migrated to Jakarta EE 9 stack

--- examples/src/main/java/my/wurflcloud/example/MyCloudClientServlet.java
+++ examples/src/main/java/my/wurflcloud/example/MyCloudClientServlet.java
@@ -4,11 +4,11 @@ package my.wurflcloud.example;
 import java.io.IOException;
 import java.util.logging.Logger;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import com.scientiamobile.wurflcloud.CloudClientLoader;
 import com.scientiamobile.wurflcloud.ICloudClientManager;
