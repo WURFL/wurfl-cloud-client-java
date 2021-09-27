44af95a5ed2dfc1219ecdadb4f5591854169c1c5
andrea <castello.a@gmail.com>
2021-09-27 11:01:14 +0200

    CCLJ-48: added license header to file

--- code/src/test/java/com/scientiamobile/wurflcloud/JSONCookieSerdeTest.java
+++ code/src/test/java/com/scientiamobile/wurflcloud/JSONCookieSerdeTest.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2015 ScientiaMobile Inc.
+ *
+ * The WURFL Cloud Client is intended to be used in both open-source and
+ * commercial environments. To allow its use in as many situations as possible,
+ * the WURFL Cloud Client is dual-licensed. You may choose to use the WURFL
+ * Cloud Client under either the GNU GENERAL PUBLIC LICENSE, Version 2.0, or
+ * the MIT License.
+ *
+ * Refer to the COPYING.txt file distributed with this package.
+ */
 package com.scientiamobile.wurflcloud;
 
 import static org.testng.Assert.assertNotNull;
